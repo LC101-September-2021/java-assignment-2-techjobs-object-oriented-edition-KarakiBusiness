ab318ec64a6fcbea9c03ac4faf6b7cef7b2bccaf
KarakiBusiness <93557122+KarakiBusiness@users.noreply.github.com>
2022-02-07 19:39:57 -0500

    second psuh

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -25,26 +25,10 @@ public abstract class JobField {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public static int getNextId() {
-        return nextId;
-    }
-
-    public static void setNextId(int nextId) {
-        JobField.nextId = nextId;
-    }
 
     public String getValue() {
         return value;
     }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
 //  Methods
 
     @Override
